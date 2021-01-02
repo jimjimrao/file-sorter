3402d7b41886d042854b883772abb3658876ea37
jimjimrao <jimmyrao2000@gmail.com>
2021-01-02 14:50:54 -0800

    Create main.py
    sorts files into individual folders in the form: day_month_year

---
+++ main.py
@@ -0,0 +1,60 @@
+#sorts files into individual folders in the form: day_month_year
+
+import os, time, datetime, platform , glob, shutil 
+#pip install module
+
+path = '/Users/jimmyrao/Desktop/screenshots_copy/'
+filenames = glob.glob(path+'*')
+# print(filenames)
+dictionary = {}
+#dicitonary = {filename: creation_time}
+
+def mac_file(file): 
+    return datetime.datetime.fromtimestamp(os.stat(file).st_birthtime)
+
+def win_file(file):
+    return datetime.datetime.fromtimestamp(os.path.getctime(file))
+
+#testing
+file = '/Users/jimmyrao/Desktop/jimmy_kang.jpg'
+print(mac_file(file))
+file_time = mac_file(file)
+
+
+
+#check operating system (windows or mac)
+osys = platform.system()
+print(osys)
+if osys == 'Windows':
+    operating_system = 'win'
+elif osys == 'Darwin':
+    operating_system = 'mac'
+
+for file in filenames:
+    if operating_system == 'win':
+        file_time = win_file(file)
+    elif operating_system == 'mac':
+        file_time = mac_file(file)
+    file_date = file_time.strftime("%d %m %Y")
+    dictionary[file] = file_date
+
+# print(dictionary)
+log = ''
+
+for key, value in dictionary.items():
+    if not os.path.exists(path+value):              # if the directory for the specific date DNE,
+        os.makedirs(path+value)                      # make a new folder for that directory
+    filename = key.split('/')
+    filename = filename[-1]
+    if not os.path.exists(path+value+'/'+filename):
+        print(path+value+'/'+filename)
+        if os.path.isfile(key):
+            shutil.move(key,path+value+'/'+filename)
+            log = log + key + ' moved to ' + path+value+'/'+filename + '\n' + '\n'
+        elif os.path.isdir(key):
+            shutil.copytree(key, path+value+'/'+ filename)
+            shutil.rmtree(key)
+            log = log + key + ' moved to ' + path+value+'/'+filename + '\n' + '\n'
+ 
+with open(path+'log.txt','w') as f:
+    f.write(log)
